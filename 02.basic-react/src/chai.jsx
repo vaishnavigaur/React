ee0acc7300461c1cdffe239a6c9fe63af63ca7f8
unknown <vaishnaviknipss023@gmail.com>
2026-06-24 09:04:17 +0530

    changes

---
+++ 02.basic-react/src/chai.jsx
@@ -0,0 +1,16 @@
+
+
+import Chai from "./chai"
+
+function chai() {
+    return(
+        <>
+         <Chai/>
+         <h1>Chai aur react</h1>
+         <p>text para</p>
+        
+        </>
+    )
+}
+
+export default App
